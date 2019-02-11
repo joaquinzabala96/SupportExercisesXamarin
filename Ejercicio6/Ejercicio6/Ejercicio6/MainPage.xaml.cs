9fc86c422c9636b33ac8a6a602329bb23356c58e
joaquinzabala96 <joaquinzabala96>
2019-02-11 17:16:03 +0100

    Ejercicio 6 completado

---
+++ Ejercicio6/Ejercicio6/Ejercicio6/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Labelsillo.TranslateTo(0, 100, 250);
+            await Labelsillo.RotateTo(360, 500, Easing.SinOut);
+            await Labelsillo.RotateTo(-360, 500, Easing.SinOut);
+            await Labelsillo.TranslateTo(0, -100, 250);
+        }
+    }
+}
