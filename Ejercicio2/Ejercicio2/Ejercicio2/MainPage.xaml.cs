2b428d2ee23259e996b7844c66fc25e8db8e8156
joaquinzabala96 <joaquinzabala96>
2019-02-11 16:31:35 +0100

    Ejercicio 2 terminado

---
+++ Ejercicio2/Ejercicio2/Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio2
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage(new Page());
+            IsPresented = false;
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page1());
+            IsPresented = false;
+        }
+        private void Button_Clicked2(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page2());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked3(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page3());
+            IsPresented = false;
+        }
+       
+    }
+}
