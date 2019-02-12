0ff9e60e6a3f42e0a1d238ce6e2964743652c321
joaquinzabala96 <joaquinzabala96>
2019-02-12 10:25:38 +0100

    Ejercicio 9 completado

---
+++ Ejercicio9/Ejercicio9/Ejercicio9/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio9
+{
+    public class Constants
+    {
+        public static string Username = "Flash";
+        public static string Password = "flash";
+    }
+}
---
+++ Ejercicio9/Ejercicio9/Ejercicio9/LoginPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio9
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+        async void Login_Clicked(object sender, EventArgs e)
+        {
+            var user = new User
+            {
+                Username = UserNameEntry.Text,
+                Password = PasswordEntry.Text
+            };
+
+            var isVaild = AreCredentialsCorrect(user);
+            if (isVaild)
+            {
+                App.IsUserLoggedIn = true;
+                Navigation.InsertPageBefore(new MainPage(), this);
+                await Navigation.PopAsync();
+            }
+            else
+            {
+                if (user.Username != Constants.Username)
+                {
+                    LoginFaild.Text = "Error User";
+                    UserNameEntry.Text = string.Empty;
+                    UserNameEntry.BackgroundColor = Color.Red;
+                }
+
+                if (user.Password != Constants.Password)
+                {
+                    LoginFaild2.Text = "Error Password";
+                    PasswordEntry.Text = string.Empty;
+                    PasswordEntry.BackgroundColor = Color.Red;
+                }
+
+            }
+        }
+
+        bool AreCredentialsCorrect(User user)
+        {
+            return user.Username == Constants.Username && user.Password == Constants.Password;
+
+        }
+    }
+}
