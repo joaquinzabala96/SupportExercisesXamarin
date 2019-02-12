96a997496e78593b7815cdbd349da3a37cc629b8
joaquinzabala96 <joaquinzabala96>
2019-02-12 10:02:24 +0100

    Ejercicio 8 completado

---
+++ Ejercicio8/Ejercicio8/Ejercicio8/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void cambio(object sender, TextChangedEventArgs e)
+        {
+            segundo.Placeholder = primero.Text;
+        }
+    }
+}
