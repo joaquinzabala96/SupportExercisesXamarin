f477f213334a42c2b2475a01676a51bf416fbbe6
joaquinzabala96 <joaquinzabala96>
2019-02-12 10:52:00 +0100

    Ejercicio 12 completado

---
+++ Ejercicio12/Ejercicio12/Ejercicio12/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio12
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Cliclar(object sender, EventArgs e)
+        {
+
+            var label = new Label { Text = "label creado", FontSize = 20 };
+            aqui.Children.Add(label);
+            this.Content = aqui;
+        }
+    }
+}
