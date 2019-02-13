2f11acb6811284c0a4395270ddd1a2f97349ed45
joaquinzabala96 <joaquinzabala96>
2019-02-13 20:26:41 +0100

    Ejercicio 15 completado

---
+++ Ejercicio15/Ejercicio15/Ejercicio15/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Xamarin.Forms;
+
+namespace Ejercicio15
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ActivityIndicator.IsRunning = true;
+
+            waitForMinute();
+        }
+
+        public void waitForMinute()
+        {
+            Device.StartTimer(new TimeSpan(0, 0, 5), () => {
+                ActivityIndicator.IsRunning = false;
+                return ActivityIndicator.IsRunning;
+            });
+
+        }
+    }
+}
