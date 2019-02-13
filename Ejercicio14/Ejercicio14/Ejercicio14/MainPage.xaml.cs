c09ec0ad7a64b0ba1678c69e120e8bdf5f63ade6
joaquinzabala96 <joaquinzabala96>
2019-02-13 20:20:39 +0100

    Ejercicio 14 completado

---
+++ Ejercicio14/Ejercicio14/Ejercicio14/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio14
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Exit", "Estas seguro amigo?", "Y", "N");
+                if (result) await this.Navigation.PopAsync();
+            });
+            return true;
+        }
+    }
+}
